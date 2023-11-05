6c400870fd3bbafd9cbd39b0f4562516bc080de6
John Higgins <johnmatthiggins@gmail.com>
2023-11-05 13:24:55 -0800

    added mps as computing option for m1

--- tetris.py
+++ tetris.py
@@ -19,7 +19,24 @@ from gameboy import GBGym
 env = GBGym()
 
 # if GPU is to be used
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+device = "cpu"
+
+if not torch.backends.mps.is_available():
+    if not torch.backends.mps.is_built():
+        print("MPS not available because the current PyTorch install was not "
+              "built with MPS enabled.")
+    else:
+        print("MPS not available because the current MacOS version is not 12.3+ "
+              "and/or you do not have an MPS-enabled device on this machine.")
+
+if torch.backends.mps.is_available():
+    device = "mps"
+elif torch.cuda.is_available():
+    device = "cuda"
+
+print('Using "%s" for device' % device)
+
+torch.device(device)
 
 Transition = namedtuple("Transition", ("state", "action", "next_state", "reward"))
 
