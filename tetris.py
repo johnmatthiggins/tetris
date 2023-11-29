6cc9b8f6e4401bdecc285932b90a5ac34f80621c
John Higgins <johnmatthiggins@gmail.com>
2023-11-29 06:41:20 -0800

    trained on 10000 iterations of the new model

--- tetris.py
+++ tetris.py
@@ -48,7 +48,7 @@ def main():
     optimizer = optim.AdamW(policy_net.parameters(), lr=LR, amsgrad=True)
     memory = ReplayMemory(MEMORY_SIZE)
 
-    num_episodes = 2000
+    num_episodes = 10000
 
     for i_episode in range(num_episodes):
         episode_score = 0
